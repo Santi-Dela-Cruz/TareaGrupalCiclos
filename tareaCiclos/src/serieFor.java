e8fb1292ab88e0328489b72a3e9e344944bd7b2f
Santi-Dela-Cruz <santidelacruzdev@gmail.com>
2024-05-15 02:35:10 -0500

    Cambio de codigo en serieFor.java

--- tareaCiclos/src/serieFor.java
+++ tareaCiclos/src/serieFor.java
@@ -1,3 +1,36 @@
 public class serieFor {
+    //Series de Santiago
+    public static void main(String[] args) {
+        forSerieK(9, '*');
+    }
 
+    public static void forSerieK(int tamano, char caracter){
+        /* Objetivo
+            
+            *     *
+            *   *
+            * * 
+            *   *
+            *     *
+            
+             */
+            if (tamano < 3) {
+                System.out.println("El tamaño debe ser al menos 3.");
+                return;
+            }
+            
+            for (int i = 0; i < tamano; i++) {
+                for (int j = 0; j <= 2 * (tamano / 2); j++) {
+                    if (j == 0) {
+                        System.out.print(caracter);
+                    }
+                    else if ((i + j == tamano / 2) || (i - j == tamano / 2)) {
+                        System.out.print(caracter);
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+    }
 }
